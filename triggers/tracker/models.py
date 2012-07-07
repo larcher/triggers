398cf6fcf1e177fd7d1ca90e849372b66376ff16
Larry Archer <larcher@spoon.cx>
2012-07-07 09:20:00 -0500

    Actually, I like those names better (Thing and DataPoint). Also, tests.

--- triggers/tracker/models.py
+++ triggers/tracker/models.py
@@ -4,21 +4,21 @@ from djangotoolbox.fields import DictField
 # Create your models here.
 
 
-class TrackedObject(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length=255)
     # TODO owner
     # TODO units
 
     def get_latest_value(self):
         # find the most recent value for this object
-        return ObjectValue.objects.filter(tracked_object=self).order_by('datetime_stamp').reverse()[0]
+        return DataPoint.objects.filter(thing=self).order_by('datetime_stamp').reverse()[0]
 
     def get_previous_change(self):
-        return ObjectValue.objects.filter(tracked_object=self).order_by('datetime_stamp').reverse()[1]
+        return DataPoint.objects.filter(thing=self).order_by('datetime_stamp').reverse()[1]
 
-class ObjectValue(models.Model):
+class DataPoint(models.Model):
     datetime_stamp = models.DateTimeField(auto_now_add=True, null=True)
     value = DictField()
     dirty = models.BooleanField(default=True)
-    tracked_object = models.ForeignKey(TrackedObject)
+    thing = models.ForeignKey(Thing)
 
--- triggers/tracker/tests.py
+++ triggers/tracker/tests.py
@@ -6,6 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models import Thing,DataPoint
 
 
 class SimpleTest(TestCase):
@@ -14,3 +15,39 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class ObjectsAndValuesTest(TestCase):
+    def setUp(self):
+        thing1 = Thing(name="Thing1")
+        thing1.save()
+        thing1 = Thing.objects.get(name="Thing1")
+
+        thing2 = Thing(name="Thing2")
+        thing2.save()
+        thing2 = Thing.objects.get(name="Thing2")
+        
+        v = DataPoint(value={"state":"open"}, thing=thing1)
+        v.save()
+        v = DataPoint(value={"state":"closed"}, thing=thing1)
+        v.save()
+        v = DataPoint(value={"state":"frobbed"}, thing=thing1)
+        v.save()
+        v = DataPoint(value={"state":"blipped"}, thing=thing2)
+        v.save()
+
+
+    def tearDown(self):
+        pass
+
+    def test_get_latest_value(self):
+        thing1 = Thing.objects.get(name='Thing1')
+        latest = thing1.get_latest_value() 
+        self.assertTrue(latest.value.has_key('state'))
+        self.assertEqual(latest.value['state'], 'frobbed')
+
+    def test_get_previous_change(self):
+        thing1 = Thing.objects.get(name='Thing1')
+        latest = thing1.get_previous_change() 
+        self.assertTrue(latest.value.has_key('state'))
+        self.assertEqual(latest.value['state'], 'closed')
+
