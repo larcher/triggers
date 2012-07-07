70f9c47aa51b868b4f3995554c72600e79068ec2
Larry Archer <larcher@spoon.cx>
2012-07-07 09:13:04 -0500

    Some basic models .. things & datapoints.

--- triggers/tracker/models.py
+++ triggers/tracker/models.py
@@ -3,3 +3,22 @@ from djangotoolbox.fields import DictField
 
 # Create your models here.
 
+
+class TrackedObject(models.Model):
+    name = models.CharField(max_length=255)
+    # TODO owner
+    # TODO units
+
+    def get_latest_value(self):
+        # find the most recent value for this object
+        return ObjectValue.objects.filter(tracked_object=self).order_by('datetime_stamp').reverse()[0]
+
+    def get_previous_change(self):
+        return ObjectValue.objects.filter(tracked_object=self).order_by('datetime_stamp').reverse()[1]
+
+class ObjectValue(models.Model):
+    datetime_stamp = models.DateTimeField(auto_now_add=True, null=True)
+    value = DictField()
+    dirty = models.BooleanField(default=True)
+    tracked_object = models.ForeignKey(TrackedObject)
+
